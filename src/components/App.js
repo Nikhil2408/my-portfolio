a40045c6f1439847b792392e9ec945873a1530b0
Nikhil Bansal <nikbansal1998@gmail.com>
2022-05-28 12:25:41 +0530

    Improved Experience section and added Education

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import '../styles/App.css';
 
 import NavBar from "./NavBar/NavBar";
 import AboutMe from './AboutMe/AboutMe';
-// import Education from './Education/Education';
+import Education from './Education/Education';
 import Certification from "./Certification/Certification.js";
 import Skills from "./Skills/Skills";
 import Experience from './Experience/Experience';
@@ -14,10 +14,10 @@ function App() {
       <NavBar />
       <AboutMe />
       <Certification />
-      <Skills />
-      <Projects />
       <Experience />
-      {/* <Education /> */}
+      <Skills />
+      <Projects />  
+      <Education />
     </div>
   );
 }
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,20 +1,29 @@
 import React from "react";
 
 import "../../styles/Experience.css";
+import accenture_logo from "../../images/accenture_logo.jpg";
 
 function Experience(){
     return (
         <div id="Experience" className="Experience">
             <h2 className="Experience-header">Experience</h2>
-            <div className="Exp-container">
-                <h2 className="Exp-companyName">Accenture</h2>
-                <ul>
-                    <li>Worked on complex queries in Oracle Database</li>
-                    <li>Worked on setting up API endpoint on AWS API Gateway and authorizing the API call with Cognito Authorizer</li>
-                    <li>Worked on a batch job to modify it to send the notifications to candidates for different exams based on email template </li>
-                    <li>Worked on setting up batch jobs through a software - Active Batch Console</li>
-                    <li>Worked as a production support engineer to resolve production issues</li>
-                </ul>
+            <div className="Exp-cLogocName">
+                    <img class="Experience-cLogo" src={accenture_logo} alt="Accenture Logo" />
+                    <h2 className="Experience-cName">Accenture</h2>
+                </div>
+            <div className="Experience-container">
+                <h3>Software Engineer</h3>
+                <p>Dec 2020 - May 2022</p>
+                <div className="something">
+                    <ul>
+                        <li>Wrote multiple complex queries in Oracle database to deliver client reports</li>
+                        <li>Working on setting up API endpoint on AWS API Gateway and authorizing the API call with Cognito Authorizer</li>
+                        <li>Currently working on documenting the API Gateway API endpoint on Swagger and resolving CORS error</li>
+                        <li>Worked on a spring batch job to modify it to send the email notifications to candidates for different exams based on email template</li>
+                        <li>Worked on setting up batch jobs through a software - Active Batch Console</li>
+                        <li>Worked as a production support engineer to resolve production issues</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
